0c633422a18c5dd550e4edf4083d6e6029bcdf92
Chris Loew <cloew123@gmail.com>
2015-04-01 17:21:59 -0600

    Added Boilerplate Package code

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='kao_xobj',
+      version='0.0.1',
+      description="",
+      author='',
+      author_email='',
+      packages=['kao_xobj'],
+     )
