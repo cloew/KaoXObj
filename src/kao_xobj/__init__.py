f07625c607037cf9b945f8cd9e76a366642e50a4
Chris Loew <cloew123@gmail.com>
2015-04-03 10:34:52 -0600

    Added methods to allow setting of cross-object attributes

--- src/kao_xobj/__init__.py
+++ src/kao_xobj/__init__.py
@@ -12,3 +12,19 @@ def xobj_attrgetter(attrName):
     def getValue(obj):
         return xobj_getattr(obj, attrName)
     return getValue
+
+def xobj_setattr(obj, attrName, value):
+    """ Set the value for the attr given following any .attrs """
+    attrs = attrName.split('.')
+    attrs, finalAttr = attrs[:-1], attrs[-1]
+    
+    parentObj = obj
+    for attr in attrs:
+        parentObj = getattr(parentObj, attr)
+    setattr(parentObj, finalAttr, value)
+    
+def xobj_attrsetter(attrName):
+    """ Set the value for the attr given following any .attrs """
+    def setValue(obj, value):
+        return xobj_setattr(obj, attrName, value)
+    return setValue
--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(name='kao_xobj',
-      version='0.0.1',
-      description="",
-      author='',
-      author_email='',
+      version='0.0.2',
+      description="Helper library to provide simple dynamic cross-object interaction",
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
       packages=['kao_xobj'],
      )
